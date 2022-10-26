bba77a7852ce6746daa5c4c06e16e45032bb325d
Vasili Karaev <kratos661@gmail.com>
2022-10-26 22:46:09 +0300

    initialize project

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// This import is marked as unused.
+use std::io::Result;
+
+// This one is not.
+use std::io::Read;
+
+// This one is.
+use std::io::Error;
+
+mod abc;
+
+fn main() {
+    println!("Hello, world!");
+}
